82ec95986d36a0e9762c5215f564be083ff4b12e
kensukeshouni <6532835+shouni@users.noreply.github.com>
2026-04-12 19:39:19 +0900

    Refactor: Reduce `defaultSegmentRateLimit` in `NewVoiceAdapter` for improved processing efficiency

--- internal/adapters/voice.go
+++ internal/adapters/voice.go
@@ -14,12 +14,11 @@ import (
 const (
 	defaultMaxParallelSegments = 5
 	defaultSegmentTimeout      = 180 * time.Second
-	defaultSegmentRateLimit    = 2000 * time.Millisecond
+	defaultSegmentRateLimit    = 1000 * time.Millisecond
 )
 
 // NewVoiceAdapter は、ports.EngineRunnerを初期化します。
 func NewVoiceAdapter(ctx context.Context, httpClient httpkit.Requester, writer remoteio.Writer) (ports.EngineRunner, error) {
-	// 1. ctxの初期化
 	engineRunner, err := builder.New(
 		ctx,
 		httpClient,
